85911c42f1bd510075f9ef33f576c73c871c8c06
s28581 <s28581@pjwstk.edu.pl>
2024-04-16 13:42:11 +0200

    something is added

--- NAI/NAI mp4 naive bayes/src/InputObject.java
+++ NAI/NAI mp4 naive bayes/src/InputObject.java
@@ -1,3 +1,11 @@
-public class InputObject {
+import java.util.ArrayList;
 
+public class InputObject {
+    ArrayList<String> attributes;
+    String group;
+
+    public InputObject(ArrayList<String> attributes, String group) {
+        this.attributes = attributes;
+        this.group = group;
+    }
 }
--- NAI/NAI mp4 naive bayes/src/Main.java
+++ NAI/NAI mp4 naive bayes/src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+    public ArrayList<InputObject> importData(String path){
+        ArrayList<InputObject> result = new ArrayList<>();
+        File file = new File(path);
+        try {
+            Scanner sc = new Scanner(file);
+            while(sc.hasNext()){
+                String[] attributesWithGroup = sc.next().split(",");
+                ArrayList<String> attributes = new ArrayList<>();
+                for(int i = 0; i < attributesWithGroup.length - 1;i++){
+                    attributes.add(attributesWithGroup[i]);
+                }
+                InputObject io = new InputObject(attributes,attributesWithGroup[attributesWithGroup.length - 1]);
+                result.add(io);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        return result;
     }
 }
